f552499680958cbcc9069460cafac433e2a6b06d
Haak Saxberg <haak.erling@gmail.com>
2015-02-27 15:51:38 -0800

    HTMLTranslator doesn't need to be a class yet; object is fine

--- src/main/scala/com/haaksmash/hst/translators/HTMLTranslator.scala
+++ src/main/scala/com/haaksmash/hst/translators/HTMLTranslator.scala
@@ -2,6 +2,6 @@ package com.haaksmash.hst.translators
 
 import com.haaksmash.hst.Document
 
-class HTMLTranslator extends BaseTranslator {
+object HTMLTranslator extends BaseTranslator {
   override def translate(document: Document): String = ???
 }
