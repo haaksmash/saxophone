9828ac8a48439a075d0bf13762b95b7e218caf9f
Haak Saxberg <haak.erling@gmail.com>
2015-02-26 20:39:00 -0800

    FileIntake works too!

--- src/main/scala/com/haaksmash/hst/intakes/FileIntake.scala
+++ src/main/scala/com/haaksmash/hst/intakes/FileIntake.scala
@@ -1,5 +1,12 @@
 package com.haaksmash.hst.intakes
 
-class FileIntake {
+object FileIntake {
+  def apply(filename:String) = {
 
+    val article_source = scala.io.Source.fromFile(filename)
+    val the_article = article_source.mkString
+    article_source.close()
+
+    StringIntake(the_article)
+  }
 }
--- src/test/scala/com/haaksmash/hst/SmokeSpec.scala
+++ src/test/scala/com/haaksmash/hst/SmokeSpec.scala
@@ -1,11 +1,12 @@
 package com.haaksmash.hst
 
-import com.haaksmash.hst.intakes.StringIntake
+import com.haaksmash.hst.intakes.{FileIntake, StringIntake}
 import org.scalatest._
 
 trait Article extends SuiteMixin { this: Suite =>
+  val article_filename = "/Users/haak/Dropbox/git_projects/personal_site/app/utility/article_example.hs"
   val article = {
-    val article_source = scala.io.Source.fromFile("/Users/haak/Dropbox/git_projects/personal_site/app/utility/article_example.hs")
+    val article_source = scala.io.Source.fromFile(article_filename)
     val the_article = article_source.mkString
     article_source.close()
     the_article
@@ -14,8 +15,11 @@ trait Article extends SuiteMixin { this: Suite =>
 
 class SmokeSpec extends FlatSpec with Matchers with Article {
 
-  "BlockParsers" should "successfully evaluate a .hst file" in {
+  "StringIntake" should "successfully evaluate a string" in {
     StringIntake(article) map { r => println(r.get) }
+  }
+  "FileIntake" should "successfully evaluate a .hst file" in {
+    FileIntake(article_filename) map { r => println(r.get) }
   }
 
 }
