55fca193040e7922a62209feb80a7bf792f8bb60
Haak Saxberg <haak.erling@gmail.com>
2015-03-25 23:41:20 -0700

    help output

--- src/main/scala/com/haaksmash/saxophone/Application.scala
+++ src/main/scala/com/haaksmash/saxophone/Application.scala
@@ -9,10 +9,19 @@ class Application {
 }
 
 object Application {
+
+  val help_string = """usage: saxophone [-d] [-o filename] input_filename"""
+
   /**
    * @param args [-d] [-o filename] input_filename
    */
   def main(args: Array[String]): Unit = {
+
+    if (args.contains("--help")) {
+      println(help_string)
+      return
+    }
+
     val saxophone_filename = args(args.length - 1)
 
     val document = FileIntake(saxophone_filename)
